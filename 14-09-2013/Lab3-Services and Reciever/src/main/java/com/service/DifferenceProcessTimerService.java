5f7cc148fde4fa0ed22d8806ad68f340275a354a
Ananyot.Ch <ananyot.ch@gmail.com>
2013-09-21 13:29:49 +0700

    update receiver

--- 14-09-2013/Lab3-Services and Reciever/src/main/java/com/service/DifferenceProcessTimerService.java
+++ 14-09-2013/Lab3-Services and Reciever/src/main/java/com/service/DifferenceProcessTimerService.java
@@ -9,13 +9,18 @@ import android.widget.Toast;
  * Created by USER on 13/9/2556.
  */
 public class DifferenceProcessTimerService extends Service {
+    public static final String NOTIFICATION = "me.picnii.receiver.broadcast.notification";
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
         }
-        Toast.makeText(this, "Timer done", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "Timer done", Toast.LENGTH_SHORT).show();
+        Intent data = new Intent(NOTIFICATION);
+        data.putExtra("Text", "Timer done");
+        sendBroadcast(data);
         return START_NOT_STICKY;
     }
 
--- 14-09-2013/Lab3-Services and Reciever/src/main/java/com/service/MainActivity.java
+++ 14-09-2013/Lab3-Services and Reciever/src/main/java/com/service/MainActivity.java
@@ -1,11 +1,21 @@
 package com.service;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, intent.getStringExtra("Text"), Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,17 @@ public class MainActivity extends Activity {
         startService(myService);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, new IntentFilter(DifferenceProcessTimerService.NOTIFICATION));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
